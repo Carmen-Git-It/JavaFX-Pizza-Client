1bb1d384c2dd93364e65b6d51357f4d3397d780a
Carmen W <Carmenwhitton@gmail.com>
2023-06-07 14:03:25 -0400

    initial commit

---
+++ src/main/java/com/carmengitit/ws02/model/Customer.java
@@ -0,0 +1,4 @@
+package com.carmengitit.ws02.model;
+
+public class Customer {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.carmengitit.ws02 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.carmengitit.ws02 to javafx.fxml;
+    exports com.carmengitit.ws02;
+}
