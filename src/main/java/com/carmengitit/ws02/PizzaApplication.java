001840b7258dffed94a5017b99ce025f2b092ec7
Carmen W <Carmenwhitton@gmail.com>
2023-06-07 15:27:15 -0400

    finish view

--- src/main/java/com/carmengitit/ws02/PizzaApplication.java
+++ src/main/java/com/carmengitit/ws02/PizzaApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class PizzaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PizzaApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(PizzaApplication.class.getResource("pizza-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Order Pizza");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/carmengitit/ws02/model/Customer.java
+++ src/main/java/com/carmengitit/ws02/model/Customer.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 public class Customer {
     private String name;
     private String phone; // Integer data types are bad at holding phone numbers.
+
     private ArrayList<Order> orders;
 
     public Customer() {
         name = "";
         phone = "";
+        orders = new ArrayList<>();
     }
 
     public Customer(String name, String phone) {
@@ -32,5 +34,17 @@ public class Customer {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
     }
 }
